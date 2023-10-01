5fc3b4a366f3742faf85a8a633ae6af83790c6c8
CJ-47 <kandari.hritik89@gmail.com>
2023-10-01 14:33:30 +0530

    2

--- CSV_Reader.py
+++ CSV_Reader.py
@@ -14,6 +14,16 @@ class Application(TkinterDnD.Tk):
         self.geometry("800x400")
         self.search_page=SearchPage(parent=self.main_frame)
 
+class DataTable(ttk.Treeview):
+    def __init__(self,parent):
+        super().__init__(parent)
+        scroll_Y=tk.Scrollbar(self,orient="vertical",command=self.yview)
+        scroll_X=tk.Scrollbar(self,orient="horizontal",command=self.xview) 
+        self.configure(yscrollcommand=scroll_Y.set,xscrollcommand=scroll_X.set)
+        scroll_Y.pack(side="right",fill="y")
+        scroll_X.pack(side="bottom",fill="x")
+        self.stored_dataframe=pd.DataFrame() 
+
 class SearchPage(tk.Frame):
     def __init__(self,parent):
         super().__init__(parent)
@@ -26,6 +36,10 @@ class SearchPage(tk.Frame):
         self.search_entrybox=tk.Entry(parent)
         self.search_entrybox.place(relx=0.25,relwidth=0.75)
         self.search_entrybox.bind("<Return")
+        
+        self.data_table=DataTable(parent)
+        self.data_table.place(rely=0.05,relx=0.25,relwidth=0.75,relheight=0.95)
+
 
 
 if __name__ =="__main__":
